b683bb869f61eb2a47df3b2106fa177d54aa594c
Kelvinjordan <kelvinfiless@gmail.com>
2019-06-20 15:28:09 +0700

    test

--- App.js
+++ App.js
@@ -8,23 +8,33 @@ export default class App extends Component {
         <View style={{flex: 1, backgroundColor: 'white'}}></View>
         <View style={{height: 50, backgroundColor: 'yellow',flexDirection: "row"}}>
           <View style={{backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{backgroundColor:'white', width: 26, height: 26}}></View>
-            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>My Profile</Text>
+            <View style={{backgroundColor:'white', width: 26, height: 26}}>
+              <Image source={require('')}
+            </View>
+            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>My Profiled</Text>
           </View>
           <View style={{backgroundColor: 'blue', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{backgroundColor:'white', width: 26, height: 26}}></View>
+            <View style={{backgroundColor:'white', width: 26, height: 26}}>
+              <Image source={require('')}
+            </View>
             <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Inbox</Text>
           </View>
           <View style={{backgroundColor: 'pink', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{backgroundColor:'white', width: 26, height: 26}}></View>
+            <View style={{backgroundColor:'white', width: 26, height: 26}}>
+              <Image source={require('')}
+            </View>
             <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Home</Text>
           </View>
           <View style={{backgroundColor: 'purple', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{backgroundColor:'white', width: 26, height: 26}}></View>
+            <View style={{backgroundColor:'white', width: 26, height: 26}}>
+              <Image source={require('')}
+            </View>
             <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Account</Text>
           </View>
           <View style={{backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <View style={{backgroundColor:'white', width: 26, height: 26}}></View>
+            <View style={{backgroundColor:'white', width: 26, height: 26}}>
+              <Image source={require('')}
+            </View>
             <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Help</Text>
           </View>
           </View>
