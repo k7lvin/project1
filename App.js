9acc4d42575f4cbfd3a86fdc073a2bb327e912ae
Kelvinjordan <kelvinfiless@gmail.com>
2019-06-20 20:22:53 +0700

    test

--- App.js
+++ App.js
@@ -1,41 +1,41 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image} from 'react-native';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{backgroundColor: 'pink', flex: 1}}>
+      <View style={{flex: 1}}>
         <View style={{flex: 1, backgroundColor: 'white'}}></View>
-        <View style={{height: 50, backgroundColor: 'yellow',flexDirection: "row"}}>
-          <View style={{backgroundColor: 'red', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{height: 50, flexDirection: "row"}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{backgroundColor:'white', width: 26, height: 26}}>
-              <Image source={require('')}
+              <Image source={require('./components/img/search.png')}/>
             </View>
-            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>My Profiled</Text>
+            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Search</Text>
           </View>
-          <View style={{backgroundColor: 'blue', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{backgroundColor:'white', width: 26, height: 26}}>
-              <Image source={require('')}
+              <Image source={require('./components/img/inbox.png')}/>
             </View>
             <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Inbox</Text>
           </View>
-          <View style={{backgroundColor: 'pink', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{backgroundColor:'white', width: 26, height: 26}}>
-              <Image source={require('')}
+              <Image source={require('./components/img/go-more.png')}/>
             </View>
-            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Home</Text>
+            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Camera</Text>
           </View>
-          <View style={{backgroundColor: 'purple', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{backgroundColor:'white', width: 26, height: 26}}>
-              <Image source={require('')}
+              <Image source={require('./components/img/account.png')}/>
             </View>
-            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Account</Text>
+            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>My Profile</Text>
           </View>
-          <View style={{backgroundColor: 'green', flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <View style={{backgroundColor:'white', width: 26, height: 26}}>
-              <Image source={require('')}
+              <Image source={require('./components/img/help.png')}/>
             </View>
-            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Help</Text>
+            <Text style={{fontSize: 10, color:'#545454', marginTop: 4}}>Account Settings</Text>
           </View>
           </View>
       </View>
